e19a60075cd008310113ea5a7f4ae7870903355c
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-09-24 21:09:24 -0300

    feat: add migration to create residues table

---
+++ database/migrations/2022_09_23_000000_create_residues_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('residues', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('type');
+            $table->string('category');
+            $table->string('technology');
+            $table->string('class');
+            $table->string('unit_measurement');
+            $table->double('weight');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('residues');
+    }
+};
